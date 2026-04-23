12fe5708351a991a9ea5380545588309c52c3bbb
Min W. Priest (they/them) <bwpriester@gmail.com>
2026-04-23 15:50:52 -0700

    moved cereal error utils into their own header

--- include/krowkee/sketch/Matrix.hpp
+++ include/krowkee/sketch/Matrix.hpp
@@ -9,61 +9,13 @@
 #include <krowkee/cereal/eigen.hpp>
 #endif
 
-#include <Eigen/Dense>
+#include <krowkee/util/eigen.hpp>
 
 #include <algorithm>
 #include <sstream>
 #include <vector>
 
-namespace krowkee::sketch::detail {
-// Eigen-compatible implementation of numpy.allclose() derived from
-// https://stackoverflow.com/questions/15051367/how-to-compare-vectors-approximately-in-eigen
-template <typename DerivedLHS, typename DerivedRHS>
-bool allclose(
-    const Eigen::DenseBase<DerivedLHS>    &lhs,
-    const Eigen::DenseBase<DerivedRHS>    &rhs,
-    const typename DerivedLHS::RealScalar &rtol =
-        Eigen::NumTraits<typename DerivedLHS::RealScalar>::dummy_precision() *
-        10,
-    const typename DerivedLHS::RealScalar &atol =
-        Eigen::NumTraits<typename DerivedLHS::RealScalar>::epsilon() * 100) {
-  // std::cout << "atol: " << atol << std::endl;
-  // std::cout << "rtol: " << rtol << std::endl;
-  // std::cout << "mean rhs: "
-  //           << ((rtol *
-  //                lhs.derived().array().abs().max(rhs.derived().array().abs()))
-  //                   .sum() /
-  //               lhs.size())
-  //           << std::endl;
-  // std::cout << "max error: "
-  //           << (lhs.derived() - rhs.derived()).array().abs().maxCoeff()
-  //           << std::endl;
-  return ((lhs.derived() - rhs.derived()).array().abs() <=
-          (atol +
-           rtol * lhs.derived().array().abs().max(rhs.derived().array().abs())))
-      .all();
-}
-
-template <typename DerivedLHS, typename DerivedRHS>
-double mean_absolute_error(const Eigen::DenseBase<DerivedLHS> &lhs,
-                           const Eigen::DenseBase<DerivedRHS> &rhs) {
-  return (lhs.derived() - rhs.derived()).array().abs().sum() / lhs.size();
-}
-
-template <typename DerivedLHS, typename DerivedRHS>
-double max_absolute_error(const Eigen::DenseBase<DerivedLHS> &lhs,
-                          const Eigen::DenseBase<DerivedRHS> &rhs) {
-  return (lhs.derived() - rhs.derived()).cwiseAbs().maxCoeff();
-}
-
-template <typename DerivedLHS, typename DerivedRHS>
-double min_absolute_error(const Eigen::DenseBase<DerivedLHS> &lhs,
-                          const Eigen::DenseBase<DerivedRHS> &rhs) {
-  return (lhs.derived() - rhs.derived()).cwiseAbs().minCoeff();
-}
-}  // namespace krowkee::sketch::detail
-namespace krowkee {
-namespace sketch {
+namespace krowkee::sketch {
 
 /// good reference for operator declarations:
 /// https://stackoverflow.com/questions/4421706/what-are-the-basic-rules-and-idioms-for-operator-overloading
@@ -462,5 +414,4 @@ class Matrix {
   }
 };
 
-}  // namespace sketch
-}  // namespace krowkee
+}  // namespace krowkee::sketch
