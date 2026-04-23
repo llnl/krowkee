4e83d10f17337037bd7f4ddd57d562e8ca5c9b4b
Min W. Priest (they/them) <bwpriester@gmail.com>
2026-04-23 15:41:30 -0700

    moved cereal eigen wrapper into its own header

--- include/krowkee/sketch/Matrix.hpp
+++ include/krowkee/sketch/Matrix.hpp
@@ -6,9 +6,7 @@
 #pragma once
 
 #if __has_include(<cereal/cereal.hpp>)
-#include <cereal/archives/binary.hpp>
-#include <cereal/cereal.hpp>
-#include <ygm/detail/ygm_cereal_archive.hpp>
+#include <krowkee/cereal/eigen.hpp>
 #endif
 
 #include <Eigen/Dense>
@@ -16,53 +14,6 @@
 #include <algorithm>
 #include <sstream>
 #include <vector>
-
-#if __has_include(<cereal/cereal.hpp>)
-// Based upon
-// https://stackoverflow.com/questions/22884216/serializing-eigenmatrix-using-cereal-library
-namespace cereal {
-// cereal for Eigen::PlainObject binary archive
-template <class Archive, class Derived>
-inline typename std::enable_if<
-    traits::is_output_serializable<BinaryData<typename Derived::Scalar>,
-                                   Archive>::value,
-    void>::type
-CEREAL_SAVE_FUNCTION_NAME(Archive                               &archive,
-                          Eigen::PlainObjectBase<Derived> const &array) {
-  using array_type = Eigen::PlainObjectBase<Derived>;
-  if (array_type::RowsAtCompileTime == Eigen::Dynamic) {
-    archive(array.rows());
-  }
-  if (array_type::ColsAtCompileTime == Eigen::Dynamic) {
-    archive(array.cols());
-  }
-  archive(binary_data(array.data(),
-                      array.size() * sizeof(typename Derived::Scalar)));
-}
-
-template <class Archive, class Derived>
-inline typename std::enable_if<
-    traits::is_input_serializable<BinaryData<typename Derived::Scalar>,
-                                  Archive>::value,
-    void>::type
-CEREAL_LOAD_FUNCTION_NAME(Archive                         &archive,
-                          Eigen::PlainObjectBase<Derived> &array) {
-  using array_type  = Eigen::PlainObjectBase<Derived>;
-  Eigen::Index rows = array_type::RowsAtCompileTime;
-  Eigen::Index cols = array_type::ColsAtCompileTime;
-  if (rows == Eigen::Dynamic) {
-    archive(rows);
-  }
-  if (cols == Eigen::Dynamic) {
-    archive(cols);
-  }
-  array.resize(rows, cols);
-  archive(binary_data(array.data(),
-                      static_cast<std::size_t>(
-                          rows * cols * sizeof(typename Derived::Scalar))));
-}
-}  // namespace cereal
-#endif
 
 namespace krowkee::sketch::detail {
 // Eigen-compatible implementation of numpy.allclose() derived from
